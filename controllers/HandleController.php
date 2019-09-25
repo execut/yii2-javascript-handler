7d74c4fed2aee4dc52b92b8f74869aa60cb768ee
eXeCUT <sexecut@gmail.com>
2019-09-25 15:34:04 +0300

    fix

--- controllers/HandleController.php
+++ controllers/HandleController.php
@@ -11,16 +11,18 @@ use yii\web\Response;
 
 class HandleController extends Controller
 {
-    public function actionIndex() {
+    public function actionIndex()
+    {
         $postData = \yii::$app->request->post();
         if (!empty($postData['data']) && is_array($postData['data']) && !empty($postData['data']['message']) && is_string($postData['data']['message'])) {
             foreach ($this->module->ignoredMessages as $ignoredMessage) {
-            if (strpos($postData['data']['message'], $ignoredMessage) !== false) {
-                \yii::$app->response->format = Response::FORMAT_JSON;
-                return true;
+                if (strpos($postData['data']['message'], $ignoredMessage) !== false) {
+                    \yii::$app->response->format = Response::FORMAT_JSON;
+                    return true;
+                }
             }
-        }
 
-        throw new Exception('Javascript error: ' . var_export($postData, true));
+            throw new Exception('Javascript error: ' . var_export($postData, true));
+        }
     }
 }
