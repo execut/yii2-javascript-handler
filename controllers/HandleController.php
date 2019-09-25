1c388e9751e65f31d0fce0b2d83319bdc7fa68fc
eXeCUT <sexecut@gmail.com>
2019-09-25 09:50:15 +0300

    fixed bug

--- controllers/HandleController.php
+++ controllers/HandleController.php
@@ -14,7 +14,7 @@ class HandleController extends Controller
     public function actionIndex() {
         $postData = \yii::$app->request->post();
         if (!empty($postData['data']) && is_array($postData['data']) && !empty($postData['data']['message']) && is_string($postData['data']['message'])) {
-            if (in_array($postData['message'], $this->module->ignoredMessages)) {
+            if (in_array($postData['data']['message'], $this->module->ignoredMessages)) {
                 \yii::$app->response->format = Response::FORMAT_JSON;
                 return true;
             }
